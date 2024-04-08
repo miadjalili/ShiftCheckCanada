b7726e137c95bdef0b54631a2cc5670542bf195e
miadjalili <jalilimiad@gmail.com>
2024-04-07 22:49:56 -0700

    Initial Commit

---
+++ ShiftCheck Canada/ShiftCheck_CanadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftCheck_CanadaApp.swift
+//  ShiftCheck Canada
+//
+//  Created by Miad Jalili on 2024-04-07.
+//
+
+import SwiftUI
+
+@main
+struct ShiftCheck_CanadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
